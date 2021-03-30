3a346a53de9b04c51564c87359ad086461e34fb3
ylenia <ylenia.rotalinti@gmail.com>
2021-03-30 23:01:17 +0200

    enhance user experience

--- app/src/main/java/com/example/tipcalculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculatorapp/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.tipcalculatorapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.example.tipcalculatorapp.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
@@ -15,6 +19,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.button1.setOnClickListener {
             calculateTip()
+        }
+        //to hide the soft keyboard when the user presses Enter
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode)
         }
     }
 
@@ -38,5 +45,17 @@ class MainActivity : AppCompatActivity() {
         }
         val resultFormatted = NumberFormat.getInstance().format(result)
         binding.resultView.text = getString(R.string.tipResult, resultFormatted)
+    }
+
+    //handles the soft keyboard
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
